2d0349bd3822624249d5989e7d42ed2423e1c45f
DarioSF <dario.sim.fra@gmail.com>
2024-02-07 15:47:50 +0100

    Práctcs para entregar en EC

---
+++ EC/PRACTICAS/P02/prac02cu9.asm
@@ -0,0 +1,22 @@
+#########################
+# 			#
+# 	PRACTICA 2	#
+# 			#
+#########################
+.text 0x00400000
+addi $v0, $0, 5
+syscall
+addi $t0, $v0, 0
+
+addi $v0, $0, 5
+syscall
+addi $t1, $v0, 0
+
+subu  t2, $t0, $t1
+
+addi $a0, $t2, 0
+addi $v0, $0, 1
+syscall
+
+addi $v0, $0, 10
+syscall
