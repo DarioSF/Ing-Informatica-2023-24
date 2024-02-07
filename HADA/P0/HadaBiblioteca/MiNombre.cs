c066ebf54e9ae0930f42c10a847d3d0f19a04ecf
DarioSF <dario.sim.fra@gmail.com>
2024-02-07 15:09:43 +0100

    Practica 0

---
+++ HADA/P0/HadaBiblioteca/MiNombre.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HadaBiblioteca
+{
+    public class MiNombre
+    {
+        string Nombre;
+
+        public MiNombre(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public string GetNombre()
+        {
+            return Nombre;
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("My name is {0}", GetNombre());
+        }
+
+    }
+}
